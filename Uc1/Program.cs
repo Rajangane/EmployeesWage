c34cee1c5cbc3a9b875b80c035be66028c55daeb
Rajangane <pratiksharajangane7@gmail.com>
2021-11-24 13:46:32 +0530

    checking employee present or not

---
+++ Uc1/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+     static void Main(String [] args)
+    {
+        //constatnts
+        int IS_FULL_TIME = 1;
+        Random random = new Random();
+        //computation
+        int empcheck = random.Next(0, 2);
+        if (empcheck == IS_FULL_TIME)
+        {
+            Console.WriteLine("employee is present");
+
+        }
+        else
+        {
+            Console.WriteLine("employee is not present");
+        }
+
+    }
+}
