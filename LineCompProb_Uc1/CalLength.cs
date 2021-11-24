aa307a2dbdb171f82071ff0ae8e216ab12bad00a
Rajangane <pratiksharajangane7@gmail.com>
2021-11-24 18:05:53 +0530

    calculating length of coordinates

---
+++ LineCompProb_Uc1/CalLength.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineCompProb_Uc1
+{
+    public class CalLength
+    {
+        
+        public void CalculateLength()
+        {
+            
+                int x1, x2, y1, y2;
+                Console.WriteLine("Enter values of the coordinates for first point(x1,y1)");
+                x1 = Convert.ToInt32(Console.ReadLine());
+                y1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter values of the coordinates for second point(x2,y2)");
+                x2 = Convert.ToInt32(Console.ReadLine());
+                y2 = Convert.ToInt32(Console.ReadLine());
+                float length;
+                length = (float)(Math.Sqrt((Math.Pow(x2 - x1, 2)) + (Math.Pow(y2 - y1, 2))));
+                Console.WriteLine("Length of the line is: " + length);
+            
+
+        }
+    }
+}
---
+++ LineCompProb_Uc1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//namespace LineCompProb_Uc1
+//{
+//    CalLength calLength = new CalLength();
+
+//}
+using LineCompProb_Uc1;
+
+new CalLength().CalculateLength();
