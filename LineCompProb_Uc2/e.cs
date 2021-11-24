999cf12b59bb4c6ab673d5eae2b96e8ea14efd3e
Rajangane <pratiksharajangane7@gmail.com>
2021-11-24 18:12:23 +0530

    checking equality of two lines based on end points

---
+++ LineCompProb_Uc2/e.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//namespace LineCompProb_Uc2;
+//{
+//  EqualCheck equal = new EqualCheck();
+//}
+
+using LineCompProb_Uc2;
+
+new EqualCheck().CalEquCheck();
+
+
+
